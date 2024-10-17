1a2d47c5d0deabbd5e7c957ad8f973a8f5406ba0
Abhishek Sood <nyp.abhishek@gmail.com>
2024-10-17 13:56:18 +0530

    first

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx}', // This ensures that Tailwind applies to all relevant files in your `src` directory.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
